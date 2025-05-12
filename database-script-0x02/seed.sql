fea28a22186d23757b118aaf2d6f869c35a7784f
Calistus Nnachebe <calistusumehnnachebe@gmail.com>
2025-05-12 09:48:29 +0100

    Update seed.sql

--- database-script-0x02/seed.sql
+++ database-script-0x02/seed.sql
@@ -13,4 +13,19 @@ VALUES
 -- Insert into Booking table
 INSERT INTO Booking (booking_id, property_id, user_id, start_date, end_date, total_price, status, created_at)
 VALUES 
-     ('john.doe@example.com'), '2025-07-01', '2025-07-10', 3000.00, 'pending', '2025-06-01'),
+    ('john.doe@example.com'), '2025-07-01', '2025-07-10', 3000.00, 'pending', '2025-06-01');
+
+-- Insert into payment
+INSERT INTO Payment (payment_id, booking_id, amount, payment_date, payment_method)
+VALUES
+(3000, '2025-06-01', 'Paypal');
+
+-- Insert into Reviews
+INSERT INTO Reviews (review_id , property_id , user_id , rating , comment , created_at)
+VALUES 
+(2 , 'very good' , '2025-07-06');
+
+-- Insert into Message 
+INSERT INTO Message (message_id , sender_id , recipient_id , message_body , sent_at)
+VALUES
+('nice to meet u' , '2025-01-02');
