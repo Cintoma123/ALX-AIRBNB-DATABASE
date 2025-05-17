bb11d6d415d0c99c242a7c1a35d5e7ed305cabbd
Calistus Nnachebe <calistusumehnnachebe@gmail.com>
2025-05-17 12:42:10 +0100

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,18 +1,15 @@
--- index- on Users 
-CREATE INDEX phone_number ON Users(last_name);
--- explain on users 
-
-EXPLAIN SELECT * FROM Users 
-WHERE phone_number
-
+ CREATE INDEX phone_number ON Users(last_name);
 -- create index for property 
 CREATE INDEX property_id ON Property(property_id);
--- explain the property 
-EXPLAIN SELECT * FROM Property
-WHERE property_id
-
 -- create index for Booking
 CREATE INDEX total_price ON Booking(total_price);
 -- explain booking
-ANALYZE SELECT * FROM Booking 
-WHERE total_price;
+-- analyze the performane
+
+EXPLAIN ANALYZE SELECT * FROM Users
+INNER JOIN Booking ON 
+Users.user_id = Booking.user_id
+
+EXPLAIN ANALYZE SELECT * FROM Users
+INNER JOIN Booking ON 
+Users.user_id = Booking.user_id
