44f2aa243eaea7bc2325040398242961cf20e7ee
Calistus Nnachebe <calistusumehnnachebe@gmail.com>
2025-05-17 10:10:38 +0100

    Update performance.sql

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -1,5 +1,17 @@
--- retrieving of all data and analyzing it 
-EXPLAIN  SELECT Booking.booking_id , Users.first_name, Users.last_name, Property.description, Payment.amount, Payment.payment_date
+-- retrieving of all data 
+SELECT Booking.booking_id , Users.first_name, Users.last_name, Property.description, Payment.amount, Payment.payment_date
+FROM  Booking
+INNER JOIN Users
+ON 
+Booking.user_id = Users.user_id
+INNER JOIN Property 
+ON 
+Users.created_at = Property.created_at
+INNER JOIN Payment 
+ON 
+Booking.booking_id = Payment.booking_id;
+-- explaining of data
+EXPLAIN SELECT Booking.booking_id , Users.first_name, Users.last_name, Property.description, Payment.amount, Payment.payment_date
 FROM  Booking
 INNER JOIN Users
 ON 
