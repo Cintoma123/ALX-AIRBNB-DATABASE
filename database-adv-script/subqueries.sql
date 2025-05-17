143dad98be0dd3f6ed737b5b84c164fc2cc9707f
Calistus Nnachebe <calistusumehnnachebe@gmail.com>
2025-05-17 15:38:52 +0100

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,31 @@
+SELECT
+    Property.PropertyID,
+    Property.Name,
+    Property.Location,
+    Property.Type,
+    Property.Price
+FROM
+    Property
+WHERE 
+    Property.PropertyID IN (
+        SELECT
+            BookingID
+        FROM 
+            Reveiw
+        GROUP BY
+            BookingID
+        HAVING
+            AVG(Rating) > 4.0
+    );
+
+--Find users who have made more than 3 bookings using a correlated subquery
+SELECT
+    User.UserID,
+    User.Name,
+    User.Email
+FROM
+    User
+WHERE 
+    (SELECT COUNT(*)
+    FROM Booking
+    WHERE Booking.UserID = User.UserID) > 3;￼Enter
