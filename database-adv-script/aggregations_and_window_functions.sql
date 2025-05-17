6c841d6540e451c549f8122c7f99671b6a3b4563
Calistus Nnachebe <calistusumehnnachebe@gmail.com>
2025-05-17 15:43:51 +0100

    Create aggregations_and_window_functions.sql

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,49 @@
+ SELECT
+     User.UserID,
+     User.Name AS UserName,
+COUNT(Booking.BookingID) AS TotalBookings
+FROM
+    User
+LEFT JOIN 
+    Booking
+ON 
+    User.UserID = Booking.UserID
+GROUP BY
+    User.UserID,
+    User.Name;
+
+--Rank properties based on the total number of bookings.
+--Uses RANK window function to rank properties by the total number of bookings they have received.
+SELECT
+    Property.PropertyID,
+    Property.Name AS PropertyName,
+    COUNT(Booking.BookingID) AS TotalBookings,
+    RANK() OVER (ORDER BY COUNT(Booking.BookingID) DESC)
+AS Rank
+FROM
+    Property
+LEFT JOIN
+    Booking
+ON
+    Property.PropertyID = Booking.PropertyID
+GROUP BY
+    Property.PropertyID,
+    Property.Name;
+
+--Rank properties based on the total number of bookings.
+--Uses ROW_NUMBER window function to rank properties by the total number of bookings they have received.
+SELECT
+    Property.PropertyID,
+    Property.Name AS PropertyName,
+    COUNT(Booking.BookingID) AS TotalBookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(Booking.BookingID) DESC)
+AS Rank
+FROM
+    Property
+LEFT JOIN
+    Booking
+ON
+    Property.PropertyID = Booking.PropertyID
+GROUP BY
+    Property.PropertyID,
+    Property.Name;
