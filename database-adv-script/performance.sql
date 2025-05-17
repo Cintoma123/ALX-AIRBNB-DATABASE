0567c70973c468a3788170b8f421db32165d1c56
Calistus Nnachebe <calistusumehnnachebe@gmail.com>
2025-05-17 10:59:36 +0100

    Update performance.sql

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -1,5 +1,26 @@
 -- retrieving of all data 
-SELECT Booking.booking_id , Users.first_name, Users.last_name, Property.description, Payment.amount, Payment.payment_date
+SELECT Booking.booking_id , 
+  Users.first_name, 
+  Users.last_name, 
+  Property.description, 
+  Payment.amount, Payment.payment_date
+FROM  Booking
+JOIN 
+  Users ON
+Booking.user_id = Users.user_id 
+JOIN  
+Property ON 
+Users.created_at = Property.created_at
+JOIN Payment 
+Payment ON 
+Booking.booking_id = Payment.booking_id;
+
+-- explaining of data
+EXPLAIN 
+  SELECT Booking.booking_id ,
+  Users.first_name, 
+  Users.last_name, Property.description, 
+  Payment.amount, Payment.payment_date
 FROM  Booking
 INNER JOIN Users
 ON 
@@ -10,7 +31,7 @@ Users.created_at = Property.created_at
 INNER JOIN Payment 
 ON 
 Booking.booking_id = Payment.booking_id;
--- explaining of data
+
 EXPLAIN SELECT Booking.booking_id , Users.first_name, Users.last_name, Property.description, Payment.amount, Payment.payment_date
 FROM  Booking
 INNER JOIN Users
@@ -22,5 +43,7 @@ Users.created_at = Property.created_at
 INNER JOIN Payment 
 ON 
 Booking.booking_id = Payment.booking_id;
+
+
 
 
